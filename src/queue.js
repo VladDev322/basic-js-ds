bf6941ee912b482792ec9f4b15624cfbdaeb67c1
VladDev322 <VladL322@yandex.ru>
2023-04-24 05:25:24 +0300

    feat: finish stack and queue

--- src/queue.js
+++ src/queue.js
@@ -1,4 +1,5 @@
 const { NotImplementedError } = require('../extensions/index.js');
+const { ListNode } = require('../extensions/list-node.js')
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -13,27 +14,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+
+
 class Queue {
   constructor() {
-    this.items = []
+    this.head = null;
+    this.tail = null;
   }
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.head;
   }
 
   enqueue(value) {
-    this.items.push(value)
+    const node = new ListNode(value);
+    if (!this.head) {
+      this.head = node;
+    }
+    if (this.tail) {
+      this.tail.next = node;
+    }
+    this.tail = node;
   }
 
   dequeue() {
-    if (this.items.length != 0) {
-      return this.items.pop()
+    if (!this.head) {
+      return undefined;
     }
+    const node = this.head;
+    this.head = node.next;
+    if (this.tail === node) {
+      this.tail = node.next;
+    }
+    return node.value;
   }
 }
+
 
 module.exports = {
   Queue
 };
+
--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.values = []
+  }
+
+  push(element) {
+    this.values.push(element)
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.values.length > 0) {
+      return this.values.pop()
+    } 
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.values.at(-1)
   }
 }
 
